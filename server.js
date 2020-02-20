61c63c20230dd62d269e6a47ebfda02026da818f
Timor Safadi <48487764+Timorsa@users.noreply.github.com>
2020-02-20 21:59:42 +0200

    not done

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connect db
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({
+    msg: 'wellcome to contact keeper api...'
+  })
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
